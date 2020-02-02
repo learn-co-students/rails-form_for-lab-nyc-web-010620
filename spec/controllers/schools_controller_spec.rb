daca0c741696bf797df27be4aed6d247269275b3
Miles Higbie <miles.higbie@gmail.com>
2020-02-02 10:53:23 -0500

    Done.

--- spec/controllers/schools_controller_spec.rb
+++ spec/controllers/schools_controller_spec.rb
@@ -1,6 +1,6 @@
 # require 'rails_helper'
 
-# RSpec.describe StudentsController, type: :controller do
+# RSpec.describe SchoolsController, type: :controller do
 
 #   describe "GET #index" do
 #     it "returns http success" do
